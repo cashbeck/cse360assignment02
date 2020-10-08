70520c2612f31ea110df4ea006a6149bb14b293e
Christian <Christian@DESKTOP-N1GKQ2B>
2020-10-07 17:33:37 -0700

    Gave functionality to all methods.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,35 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String str = "0";		//initial value is always 0 and built off of
   
   public AddingMachine () {
-    total = 0;  // not needed - included for clarity
+    total = 0;  	//not needed - included for clarity
   }
-  //returns the sum of total at any given point
+  
+  //returns the sum at any given point
   public int getTotal () {
-    return 0;
+    return total;
   }
   //adds an integer to the total
   public void add (int value) {
+	  total = total + value;
+	  String aT = (" + " + value);
+	  str = str + aT;
   }
   //subtracts an integer from the total
   public void subtract (int value) {
+	  total = total - value;
+	  String sT = (" - " + value); 
+	  str = str + sT;
   }
   //shows the operations that have been performed in a single string
   public String toString () {
-    return "";
+    return str;
   }
   //clears operations performed and resets total to 0
   public void clear() {
+	  total = 0;
+	  str = "0";
   }
 }
