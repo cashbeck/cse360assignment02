dfe4336aa7d767c4ca52d85df8f94627d54dcc01
Christian Ashbeck <cashbeck@asu.edu>
2020-10-07 17:26:50 -0700

    Initial commit of the assignment

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  //returns the sum of total at any given point
+  public int getTotal () {
+    return 0;
+  }
+  //adds an integer to the total
+  public void add (int value) {
+  }
+  //subtracts an integer from the total
+  public void subtract (int value) {
+  }
+  //shows the operations that have been performed in a single string
+  public String toString () {
+    return "";
+  }
+  //clears operations performed and resets total to 0
+  public void clear() {
+  }
+}
